f2508eb563519be93e8a1c069ab9bf480d919bc7
Carlos <chdezmar@gmail.com>
2016-06-21 15:18:58 +0100

    Jasmine testing, spies

---
+++ spec/AirportSpec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Airport', function(){
+	var airport;
+	var plane;
+
+	beforeEach(function() {
+		airport = new Airport();
+		plane = jasmine.createSpy('plane');
+	});
+
+	it('has an empty array of planes',function() {
+		expect(airport.planes()).toEqual([])
+	});
+
+	it('can clear planes for landing', function(){
+		airport.clearForLanding(plane);
+		expect(airport.planes()).toEqual([plane]);
+	});
+});
